0b652cc64711c60ea223d03568868f4c1d6dc5a0
hungcoj <bach.dang.hung@framgia.com>
2016-12-27 14:58:48 +0700

    Auth login logout

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,21 @@ require('laravel-elixir-vue-2');
  |
  */
 
-elixir((mix) => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+elixir(mix => {
+  mix
+    .copy([
+      "node_modules/bootstrap/fonts",
+      "node_modules/font-awesome/fonts",
+    ], "public/build/fonts")
+    .styles([
+      'node_modules/bootstrap/dist/css/bootstrap.min.css',
+      'node_modules/font-awesome/css/font-awesome.min.css',
+    ], 'public/css/vendor.css', './')
+    .webpack("app.js")
+    .webpack("vendor.js")
+    .sass("app.scss")
+    .version([
+      "css/*.css",
+      "js/*.js"
+    ]);
 });
